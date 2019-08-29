cc5eb3aeb45a289646fa1ac45017911ffec0fe9f
Stdio <kwork-stdio@mail.ru>
2019-08-29 14:28:08 +0300

    0.0.1

--- app/src/main/java/com/stdio/chinatranslator/MainActivity.java
+++ app/src/main/java/com/stdio/chinatranslator/MainActivity.java
@@ -1,14 +1,236 @@
 package com.stdio.chinatranslator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pedromassango.doubleclick.DoubleClick;
+import com.pedromassango.doubleclick.DoubleClickListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements OnKeyboardVisibilityListener {
+
+    //API запросов написан тут https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
+    private final String KEY = "trnsl.1.1.20180923T185306Z.c0c12e833188400b.99507f30e616465853f697c9b2961fac934fd5aa";// API ключ получить можно здесь https://translate.yandex.ru/developers/keys
+    private final String URL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+
+    EditText et;
+    TextView tvTranslatedText, tvLanguage;
+    FloatingActionButton FABClear;
+    String result;
+    String currentLang = "ru-zh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setKeyboardVisibilityListener(this);
+    }
+
+    private void setKeyboardVisibilityListener(final OnKeyboardVisibilityListener onKeyboardVisibilityListener) {
+        final View parentView = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        parentView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            private boolean alreadyOpen;
+            private final int defaultKeyboardHeightDP = 100;
+            private final int EstimatedKeyboardDP = defaultKeyboardHeightDP + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 48 : 0);
+            private final Rect rect = new Rect();
+
+            @Override
+            public void onGlobalLayout() {
+                int estimatedKeyboardHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, EstimatedKeyboardDP, parentView.getResources().getDisplayMetrics());
+                parentView.getWindowVisibleDisplayFrame(rect);
+                int heightDiff = parentView.getRootView().getHeight() - (rect.bottom - rect.top);
+                boolean isShown = heightDiff >= estimatedKeyboardHeight;
+
+                if (isShown == alreadyOpen) {
+                    Log.i("Keyboard state", "Ignoring global layout change...");
+                    return;
+                }
+                alreadyOpen = isShown;
+                onKeyboardVisibilityListener.onVisibilityChanged(isShown);
+            }
+        });
+    }
+
+
+    @Override
+    public void onVisibilityChanged(boolean visible) {
+        if (visible) {
+            FABClear.hide();
+        } else {
+            FABClear.show();
+        }
+    }
+
+    private void initViews() {
+        et = findViewById(R.id.et);
+        tvTranslatedText = findViewById(R.id.tvTranslatedText);
+        tvLanguage = findViewById(R.id.tvLang);
+        FABClear = findViewById(R.id.FABClear);
+        setEditTextOnChangeListener();
+        setDoubleTapListener();
+    }
+
+    private void setDoubleTapListener() {
+        tvTranslatedText.setOnClickListener( new DoubleClick(new DoubleClickListener() {
+            @Override
+            public void onSingleClick(View view) {
+
+                // Single tap here.
+            }
+
+            @Override
+            public void onDoubleClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) MainActivity.this.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("", tvTranslatedText.getText().toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(MainActivity.this, "Перевод скопирован", Toast.LENGTH_SHORT).show();
+            }
+        }));
+    }
+
+
+
+    public void swap(View view) {
+        if (currentLang.equals("ru-zh")) {
+            currentLang = "zh-ru";
+            tvLanguage.setText("Китайский - Русский");
+        }
+        else if (currentLang.equals("zh-ru")) {
+            currentLang = "ru-zh";
+            tvLanguage.setText("Русский - Китайский");
+        }
+        et.setText(tvTranslatedText.getText().toString());
+        et.setSelection(et.getText().length());
+        translate();
+    }
+
+    public void clear(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        alertDialogBuilder
+                .setMessage("Очистить?")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        et.setText("");
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialogBuilder.setCancelable(true);
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    private void setEditTextOnChangeListener() {
+        et.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                if (et.getText().toString().isEmpty()) {
+                    tvTranslatedText.setText("");
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if (et.getText().toString().isEmpty()) {
+                    tvTranslatedText.setText("");
+                }
+                else {
+                    translate();
+                }
+            }
+        });
+    }
+
+    private void translate() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            result = jsonObject.getString("text")
+                                    .replace("[", "").replace("]", "");
+                            result = result.replaceAll("\"", "");
+                            tvTranslatedText.setText(result);
+                        } catch (JSONException e) {
+                            if (e.getMessage() != null) {
+                                Log.e("JSONException аы", e.getMessage());
+                            }
+                        }
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.getMessage() != null) {
+                            Log.e("VolleyError аы", error.getMessage());
+                        }
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("key", KEY);
+                params.put("text", et.getText().toString());
+                params.put("lang", currentLang);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/stdio/chinatranslator/OnKeyboardVisibilityListener.java
@@ -0,0 +1,5 @@
+package com.stdio.chinatranslator;
+
+public interface OnKeyboardVisibilityListener {
+    void onVisibilityChanged(boolean visible);
+}
